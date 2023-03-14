6396a4d1f26774ae3441b13dfc472b26e9e0e7dc
AnaCarvalho3 <anacarvalho3197@gmail.com>
2023-03-14 20:16:18 -0300

    laravel

---
+++ ana_carvalho/cliente-laravel/routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Http\Controllers\ClientController;
+use App\Http\Controllers\ProductController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/clients', [ClientController::class, 'index'])->name('clients.index');
+Route::get('/clients/{id}', [ClientController::class, 'show'])->name('clients.show');
+
+
+Route::get('/products', [ProductController::class, 'index'])->name('products.index');
